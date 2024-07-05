6f4d22bf597139bdff2a4c62249aa2f04f0caf6b
shravyamone <158325804+shravyamone@users.noreply.github.com>
2024-07-05 06:00:43 +0000

    comments added

--- string.py
+++ string.py
@@ -1,8 +1,9 @@
+#reverse
 def reverse(x):
   return x[::-1]
-  
+#palindrome  
 def pallindrome(x):
   return x.lower() ==reverse(x.lower())
-  
+#lowercase
 def convert_lower(x):
   return x.lower()
